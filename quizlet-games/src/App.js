196f441071a1d9d3ccfad9b3b71546d4e103903a
nathanlm511 <nathanmoeliono@gmail.com>
2020-10-06 17:12:41 -0400

    first iteration of concentration

--- quizlet-games/src/App.js
+++ quizlet-games/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Home from './Home';
+import Concentration from './Concentration';
+
+import {Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/concentration" component={Concentration} />
+        <Route path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
