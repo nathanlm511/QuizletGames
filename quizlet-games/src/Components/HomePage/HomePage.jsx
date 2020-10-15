09894322e4e2ebd02321a212399d08a66591f4fd
Eilima <ericklima34@gmail.com>
2020-10-14 22:00:16 -0500

    Add tictactoe multiplayer

--- quizlet-games/src/Components/HomePage/HomePage.jsx
+++ quizlet-games/src/Components/HomePage/HomePage.jsx
@@ -1,39 +1,41 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import { fire } from "../../Auth/firebase";
+import { db, fire } from "../../Auth/firebase";
 import "./HomePage.css";
 import { Login } from "../Login/Login";
 import { Link } from "react-router-dom";
 
-import Carousel from 'react-material-ui-carousel'
+import Carousel from "react-material-ui-carousel";
 
-import GameCard from './GameCard';
+import GameCard from "./GameCard";
 
-import { makeStyles } from '@material-ui/core/styles';
-import { Container,
-         Box,
-         Card,
-         Paper,
-         Button,
-         TextField,
-         AppBar,
-         Toolbar,
-         IconButton,
-         Typography
-         } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Container,
+  Box,
+  Card,
+  Paper,
+  Button,
+  TextField,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+} from "@material-ui/core";
+import { auth } from "firebase";
 
 const useStyles = makeStyles({
   titleCard: {
-    width: '100%',
-    backgroundColor: '#4257b2',
-    color: '#ffffff',
-    display:'flex',
-    marginBottom: '30px',
-    marginTop: '30px',
-    width: '80%',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+    width: "100%",
+    backgroundColor: "#4257b2",
+    color: "#ffffff",
+    display: "flex",
+    marginBottom: "30px",
+    marginTop: "30px",
+    width: "80%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
 
 export const HomePage = () => {
@@ -42,7 +44,8 @@ export const HomePage = () => {
   const [parsedValue, setParsedValues] = useState({});
   const [user, setUser] = useState("");
   const [signUpDisplay, setSignUpDisplay] = useState(true);
-  
+  const [cardDeckName, setCardDeckName] = useState("");
+
   useEffect(() => {
     const authListener = () => {
       fire.auth().onAuthStateChanged((user) => {
@@ -74,7 +77,6 @@ export const HomePage = () => {
     console.log(cardObject);
     setParsedValues(cardObject);
   };
-
   const startGame = () => {
     parser();
     //   Start selected game here
@@ -90,93 +92,105 @@ export const HomePage = () => {
     if (user) {
       return (
         <AppBar position="static">
-        <Toolbar>
-          <h2 className="title">Quizlet Games</h2>
-          <Button onClick={handleLogOut} color="inherit">Log Out</Button>
-        </Toolbar>
-      </AppBar>
+          <Toolbar>
+            <h2 className="title">Quizlet Games</h2>
+            <Button onClick={handleLogOut} color="inherit">
+              Log Out
+            </Button>
+          </Toolbar>
+        </AppBar>
       );
     } else {
       return (
         <AppBar position="static">
-        <Toolbar>
-          <h2 className="title">Quizlet Games</h2>
-          <Button onClick={() => {setSignUpDisplay(!signUpDisplay);}} color="inherit">
-            Sign Up / Log In
-          </Button>
-        </Toolbar>
-      </AppBar>
+          <Toolbar>
+            <h2 className="title">Quizlet Games</h2>
+            <Button
+              onClick={() => {
+                setSignUpDisplay(!signUpDisplay);
+              }}
+              color="inherit"
+            >
+              Sign Up / Log In
+            </Button>
+          </Toolbar>
+        </AppBar>
       );
     }
   };
 
   var singlePlayerGames = [
-      {
-        photo: "memory.jpg",
-        link: "/concentration",
-        name: "Concentration",
-        description: "Think you got your flash cards memorized? See how quickly you can match them up."
-      },
-      {
-        photo: "connect4.png",
-        link: "/concentration",
-        name: "Connect 4 (WIP)",
-        description: "A classic game of tactics and strategy. Connect 4 in a row to win!"
-      },
-      {
-        photo: "hangman.jpg",
-        link: "/concentration",
-        name: "Hangman (WIP)",
-        description: "Guess the answer to the flashcard before the man is hung"
-      }
-  ]
+    {
+      photo: "memory.jpg",
+      link: "/concentration",
+      name: "Concentration",
+      description:
+        "Think you got your flash cards memorized? See how quickly you can match them up.",
+    },
+    {
+      photo: "connect4.png",
+      link: "/concentration",
+      name: "Connect 4 (WIP)",
+      description:
+        "A classic game of tactics and strategy. Connect 4 in a row to win!",
+    },
+    {
+      photo: "hangman.jpg",
+      link: "/concentration",
+      name: "Hangman (WIP)",
+      description: "Guess the answer to the flashcard before the man is hung",
+    },
+  ];
 
   var multiPlayerGames = [
-      {
-        photo: "tic.png",
-        link: "/concentration",
-        name: "Tic-Tac-Toe (WIP)",
-        description: "Tic the tac the toe. This is placeholder text FJSDIFSDFSDIFJSDFJSIDF"
-      },
-      {
-        photo: "connect4.png",
-        link: "/concentration",
-        name: "Rocket League",
-        description: "Use racecars to dribble, pass, and shoot a ball into the goal!"
-      },
-      {
-        photo: "hangman.jpg",
-        link: "/concentration",
-        name: "Pong (WIP)",
-        description: "Keep the ball out of your goal. Move the paddle back and forth to block the ball"
-      }
-  ]
+    {
+      photo: "tic.png",
+      link: "/menu",
+      name: "Tic-Tac-Toe (WIP)",
+      description:
+        "Tic the tac the toe. This is placeholder text FJSDIFSDFSDIFJSDFJSIDF",
+    },
+    {
+      photo: "connect4.png",
+      link: "/concentration",
+      name: "Rocket League",
+      description:
+        "Use racecars to dribble, pass, and shoot a ball into the goal!",
+    },
+    {
+      photo: "hangman.jpg",
+      link: "/concentration",
+      name: "Pong (WIP)",
+      description:
+        "Keep the ball out of your goal. Move the paddle back and forth to block the ball",
+    },
+  ];
 
   const bodyDisplay = () => {
     if (signUpDisplay) {
       return (
-        <Paper className = "content-container">
-        <div className="games">
-          <div className="single-player-container">
-            <p className="single">Single Player</p>
-            <Carousel autoPlay={false}>
-              {
-                  singlePlayerGames.map( (game, i) => <GameCard game={game} quizletData={parsedValue}/> )
-              }
-            </Carousel>
+        <Paper className="content-container">
+          <div className="games">
+            <div className="single-player-container">
+              <p className="single">Single Player</p>
+              <Carousel autoPlay={false}>
+                {singlePlayerGames.map((game, i) => (
+                  <GameCard game={game} quizletData={parsedValue} />
+                ))}
+              </Carousel>
+            </div>
+            <div className="multi-player-container">
+              <p className="multi">Multiplayer</p>
+              <Carousel autoPlay={false}>
+                {multiPlayerGames.map((game, i) => (
+                  <GameCard game={game} quizletData={parsedValue} />
+                ))}
+              </Carousel>
+            </div>
           </div>
-          <div className="multi-player-container">
-            <p className="multi">Multiplayer</p>
-            <Carousel autoPlay={false}>
-              {
-                  multiPlayerGames.map( (game, i) => <GameCard game={game} quizletData={parsedValue}/> )
-              }
-            </Carousel>
-          </div>
-        </div>
-        <div className="input-container">
-          <div className="text-field--container">
-            <TextField
+          <div className="input-container">
+            <div className="text-field--container">
+              <TextField
                 label="Exported Quizlet Data"
                 multiline
                 rows={8}
@@ -184,10 +198,10 @@ export const HomePage = () => {
                 defaultValue="Enter your exported data here. Look at the the tooltip for help."
                 onChange={handleChange}
                 variant="outlined"
-            />
+              />
+            </div>
           </div>
-        </div>
-      </Paper>
+        </Paper>
       );
     } else {
       return (
@@ -206,7 +220,7 @@ export const HomePage = () => {
       <div className="whitespace"></div>
       <div className="App">{bodyDisplay()}</div>
     </>
-  )
+  );
 };
 
 export default HomePage;
